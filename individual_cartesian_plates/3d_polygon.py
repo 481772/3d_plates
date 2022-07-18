9cf850d4ebee742579550f32f774c916c262ef18
Justin <79943109+481772@users.noreply.github.com>
2022-07-18 13:57:58 -0500

    Add files via upload

---
+++ individual_cartesian_plates/3d_polygon.py
@@ -0,0 +1,64 @@
+#Reads a file's radian coordinates from the directory
+input_file = open('WL.txt', 'r')
+
+
+
+
+
+import matplotlib.pyplot as plt
+
+
+lines = input_file.readlines()
+
+
+sequence_containing_x_vals = []
+sequence_containing_y_vals = []
+sequence_containing_z_vals = []
+num = 0 
+
+def Convert(string):
+    list1=[]
+    list1[:0]=string
+    return list1
+
+def append_to_n_vals():
+    global sequence_containing_x_vals 
+    global sequence_containing_y_vals
+    global sequence_containing_z_vals
+    global num
+    var = [] 
+
+    for i in range(0, coord_list.index(" ")): 
+        var.append(coord_list[0])
+        del coord_list[0]
+    del coord_list[0]
+    #delete up to/including the space but append everything before that (var) as an x coord 
+    sequence_containing_x_vals.append(float("".join(var)))
+    
+    var = []
+    for i in range(0, coord_list.index(" ")): 
+        var.append(coord_list[0])
+        del coord_list[0]
+    del coord_list[0]
+    sequence_containing_y_vals.append(float("".join(var)))
+    #code for y is identical to that of x, since it's the same case ()
+
+    var = []
+    for i in range(0, coord_list.index("\n")-1): 
+        var.append(coord_list[i])
+    sequence_containing_z_vals.append(float("".join(var)))
+
+
+for i in lines: 
+    coord_list = Convert(i)
+    append_to_n_vals()
+
+
+
+
+fig = plt.figure(figsize=(12, 12))
+ax = fig.add_subplot(projection='3d')
+
+
+ax.plot(sequence_containing_x_vals, sequence_containing_y_vals, sequence_containing_z_vals, '-o')
+plt.show()
